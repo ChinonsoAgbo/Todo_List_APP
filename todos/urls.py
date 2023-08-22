152f9fa4ae9faba8fac8aeb0a8f77dff1bc29e11
agbochinonso <cagbo@stud.hs-heilbronn.de>
2023-08-23 00:26:51 +0200

    Complete Todo

--- todos/urls.py
+++ todos/urls.py
@@ -2,12 +2,21 @@ from django.urls import path
 
 from . import views 
 urlpatterns = [
+    # button to add a task 
     path('addTask/', views.addTask, name='addTask'),
+    # button to delete a task
     path('delete/<int:task_id>/', views.deleteTask, name='deleteTask'),
+    # button to complete a task
     path('completeTask/<int:task_id>/', views.completeTask, name='completeTask'), # button to complete a task
 
+    # button to clear completed tasks
+    path('clearCompleted/', views.clearCompleted, name='clearCompleted'),
 
-    path('clearCompleted/', views.clearCompleted, name='clearCompleted'),# button to clear completed tasks
+    # button to mark as undone tasks
+    path('markAsUndone/<int:task_id>/', views.markAsUndone, name='markAsUndone'),
+    
+    # button to edit a task
+    path('editTask/<int:task_id>/', views.editTask, name='editTask'),
 
 
 ]
--- todos/views.py
+++ todos/views.py
@@ -14,13 +14,33 @@ def deleteTask(request,task_id):
     task=get_object_or_404(Task,id=task_id) #
     task.delete()
     return redirect('home-todo')
+
 # Mark a task as complete 
 def completeTask(request,task_id):
     task=get_object_or_404(Task,id=task_id) #
     task.is_completed=True
     task.save()
     return redirect('home-todo')
+
 # Mark a task as incomplete
 def clearCompleted(request):
     Task.objects.filter(is_completed=True).delete()
     return redirect('home-todo')
+
+#remark a task as incomplete
+def markAsUndone(request,task_id):
+    task=get_object_or_404(Task,id=task_id) #
+    task.is_completed=False
+    task.save()
+    return redirect('home-todo')
+
+# Edit an existing task
+def editTask(request,task_id):
+    get_task=get_object_or_404(Task,id=task_id) #
+    if request.method == 'POST':
+        get_task.task=request.POST['task']
+        get_task.save()
+        return redirect('home-todo')
+    else:
+        context = {'get_task':get_task}
+        return render(request,'edit_task.html', context)
